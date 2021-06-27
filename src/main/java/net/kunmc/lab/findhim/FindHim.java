fff1c6a50c12110cd82533f33f6171215c00bc99
fooaaa6 <85221639+fooaaa6@users.noreply.github.com>
2021-06-28 00:16:34 +0900

    Add files via upload

---
+++ src/main/java/net/kunmc/lab/findhim/FindHim.java
@@ -0,0 +1,60 @@
+package net.kunmc.lab.findhim;
+
+import org.bukkit.Bukkit;
+import org.bukkit.event.HandlerList;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scoreboard.Scoreboard;
+import org.bukkit.scoreboard.ScoreboardManager;
+
+public final class FindHim extends JavaPlugin {
+    //チーム関連
+    public static Scoreboard board;
+    ScoreboardManager sbManager;
+
+    //起動関連
+    public static boolean running = false;
+    public static FindHim plugin;
+
+
+    @Override
+    public void onEnable() {
+        System.out.println("[FindHim] Plugin ON^.＿.^");
+        plugin = this;
+
+        sbManager = Bukkit.getScoreboardManager();
+        board = sbManager.getMainScoreboard();
+
+        //各チームを設定
+        TeamSetUp.setupFinder();
+        TeamSetUp.setupTarget();
+
+        getServer().getPluginManager().registerEvents(new TouchEventListener(), this);
+        this.getCommand("findhim").setExecutor(new CommandManager());
+    }
+
+    @Override
+    public void onDisable() {
+        HandlerList.unregisterAll(this);
+    }
+
+    public void start() {
+        if (running) {
+            return;
+        }
+        running = true;
+        GameManager.setGameMode();
+        GameManager.gameManager();
+    }
+
+    public static void timer(){
+        new SchedulerTask().runTaskTimer(plugin,0L,20L);
+    }
+
+    public static void stop() {
+        GameManager.playing = false;
+        running = false;
+        GameManager.kaisuued = 0;
+        TouchEventListener.captured = false;
+        return;
+    }
+}
---
+++ src/main/java/net/kunmc/lab/findhim/SchedulerTask.java
@@ -0,0 +1,58 @@
+package net.kunmc.lab.findhim;
+
+import org.bukkit.scheduler.BukkitRunnable;
+
+public class SchedulerTask extends BukkitRunnable {
+    @Override
+    public void run() {// スケジューラ側で実行する処理の内容
+
+
+        if(!FindHim.running){
+            this.cancel();
+            GameManager.forPlayers(GameManager.modeGameSet);
+            return;
+        }
+
+        //スタート前
+        if(!GameManager.playing){
+            GameManager.forPlayers(GameManager.modeCountDown0);
+            if(GameManager.i-- <= 0){
+                this.cancel();
+                GameManager.playing = true;
+                FindHim.timer();//スケジューラでカウントダウン
+            }
+            return;
+        }
+
+        //スタート後
+        GameManager.time--;
+        if(TouchEventListener.captured){
+            if(GameManager.time>GameManager.intT){
+                GameManager.forPlayers(GameManager.modeCaptureWanted);
+            }else if(GameManager.kaisuued<GameManager.kaisuu) {
+                TouchEventListener.captured = false;
+                GameManager.gameManager();
+            }else {
+                TouchEventListener.captured = false;
+                GameManager.kaisuued = 0;
+                this.cancel();
+                GameManager.forPlayers(GameManager.modeGameSet);
+            }
+        }
+        else
+        {
+            if (GameManager.time >= GameManager.intT-5) {
+                GameManager.forPlayers(GameManager.modeShowWanted);
+            }
+            if (GameManager.time <= GameManager.intT-6) {
+                GameManager.forPlayers(GameManager.modeGamePlaying);
+            }
+            if (GameManager.time <= 0) {
+                this.cancel();
+                GameManager.forPlayers(GameManager.modeGameSet);
+            }
+        }
+    }
+
+
+}
